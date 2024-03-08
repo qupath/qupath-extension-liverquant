0408272d333dfde0a8ba96553bf698ba0418d06c
lleplat <lleplat@ed.ac.uk>
2024-03-08 13:36:13 +0000

    Improved sample script readability

--- sample-scripts/run-detection-with-parameters.groovy
+++ sample-scripts/run-detection-with-parameters.groovy
@@ -10,6 +10,31 @@ import qupath.ext.liverquant.core.*
  * click on Objects > Refresh Object Ids to see the results.
  */
 
+// Parameters. The values displayed here are the default values, so if nothing is modified,
+// running this script is equivalent to running the run-detection.groovy script
+def progressDisplay = FatGlobulesDetectorParameters.ProgressDisplay.WINDOW      // or FatGlobulesDetectorParameters.ProgressDisplay.LOG to not use the UI
+def objectToCreate = FatGlobulesDetectorParameters.ObjectToCreate.DETECTION     // or FatGlobulesDetectorParameters.ObjectToCreate.ANNOTATION to create annotations instead of detections
+def pixelSize = -1                                                  // pixel size in microns at which the detection should be performed. A negative value means using the full resolution image
+def lowerBound = new FatGlobulesDetectorParameters.HsvArray(0, 0, 200)      // the inclusive lower bound array in HSV-space that should be used for color segmentation
+def upperBound = new FatGlobulesDetectorParameters.HsvArray(180, 25, 255)   // the inclusive upper bound array in HSV-space that should be used for color segmentation
+def minIsolatedGlobuleElongation = 0.4      // the minimal elongation a shape should have to be considered as an isolated globule
+def minOverlappingGlobuleElongation = 0.05  // the minimal elongation a shape should have to be considered as an overlapping globule
+def minIsolatedGlobuleSolidity = 0.85       // the minimal solidity a shape should have to be considered as an isolated globule
+def minOverlappingGlobuleSolidity = 0.7     // the minimal solidity a shape should have to be considered as an overlapping globule
+def minDiameter = 5                            // the minimal diameter (in microns) a shape should have to be considered as a globule
+def maxDiameter = 100                          // the maximal diameter (in microns) a shape should have to be considered as an isolated globule
+def tileWidth = 512                            // the width of each tile (in pixels) at which the detection should be performed
+def tileHeight = 512                           // the height of each tile (in pixels) at which the detection should be performed
+def padding = 64                               // the padding (in pixels) of each tile at which the detection should be performed
+def boundaryThreshold = 0.5                 // Objects created on the boundaries of tiles are merged with a shared boundary IoU criterion.
+                                                       // The boundary threshold is the minimum intersection-over-union (IoU) proportion
+                                                       // of the possibly-clipped boundary for merging
+def onFinished = () -> {
+    // Everything here will be executed after the detection is complete.
+    // Note that if you print something here, you'll have to open the QuPath logs to see it (it won't show on the script editor)
+}
+
+
 def imageData = getCurrentImageData()
 if (imageData == null) {
     println "An image must be open before running this script"
@@ -23,24 +48,21 @@ if (selectedAnnotations.isEmpty()) {
 }
 
 FatGlobuleDetector.run(new FatGlobulesDetectorParameters.Builder(imageData, selectedAnnotations)
-        .setProgressDisplay(FatGlobulesDetectorParameters.ProgressDisplay.WINDOW)       // or FatGlobulesDetectorParameters.ProgressDisplay.LOG to not use the UI
-        .setObjectToCreate(FatGlobulesDetectorParameters.ObjectToCreate.DETECTION)      // or FatGlobulesDetectorParameters.ObjectToCreate.ANNOTATION to create annotations instead of detections
-        .setPixelSize(-1)          // pixel size in microns at which the detection should be performed. A negative value means using the full resolution image
-        .setLowerBound(new FatGlobulesDetectorParameters.HsvArray(0, 0, 200))   // the inclusive lower bound array in HSV-space that should be used for color segmentation
-        .setUpperBound(new FatGlobulesDetectorParameters.HsvArray(180, 25, 255))    // the inclusive upper bound array in HSV-space that should be used for color segmentation
-        .setMinIsolatedGlobuleElongation(0.4)   // the minimal elongation a shape should have to be considered as an isolated globule
-        .setMinOverlappingGlobuleElongation(0.05)   // the minimal elongation a shape should have to be considered as an overlapping globule
-        .setMinIsolatedGlobuleSolidity(0.85)    // the minimal solidity a shape should have to be considered as an isolated globule
-        .setMinOverlappingGlobuleSolidity(0.7)  // the minimal solidity a shape should have to be considered as an overlapping globule
-        .setMinDiameter(5)      // the minimal diameter (in microns) a shape should have to be considered as a globule
-        .setMaxDiameter(100)    // the maximal diameter (in microns) a shape should have to be considered as an isolated globule
-        .setTileWidth(512)      // the width of each tile (in pixels) at which the detection should be performed
-        .setTileHeight(512)     // the height of each tile (in pixel) at which the detection should be performed
-        .setPadding(64)         // the padding (in pixels) of each tile at which the detection should be performed
-        .setBoundaryThreshold(0.5)  // Objects created on the boundaries of tiles are merged with a shared boundary IoU criterion. The value passed to this function is the minimum intersection-over-union (IoU) proportion of the possibly-clipped boundary for merging
-        .setOnFinished(() -> {
-            // Everything here will be executed after the detection is complete.
-            // Note that if you print something here, you'll have to open the QuPath logs to see it (it won't show on the script editor)
-        })
+        .setProgressDisplay(progressDisplay)
+        .setObjectToCreate(objectToCreate)
+        .setPixelSize(pixelSize)
+        .setLowerBound(lowerBound)
+        .setUpperBound(upperBound)
+        .setMinIsolatedGlobuleElongation(minIsolatedGlobuleElongation)
+        .setMinOverlappingGlobuleElongation(minOverlappingGlobuleElongation)
+        .setMinIsolatedGlobuleSolidity(minIsolatedGlobuleSolidity)
+        .setMinOverlappingGlobuleSolidity(minOverlappingGlobuleSolidity)
+        .setMinDiameter(minDiameter)
+        .setMaxDiameter(maxDiameter)
+        .setTileWidth(tileWidth)
+        .setTileHeight(tileHeight)
+        .setPadding(padding)
+        .setBoundaryThreshold(boundaryThreshold)
+        .setOnFinished(onFinished)
         .build()
 )
